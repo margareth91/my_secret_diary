6a7efbcd5a000a6a2eb7a46444356679d2230bbc
margareth91 <margerytka@wp.eu>
2024-01-25 16:19:40 +0100

    added entry_list url, view & template

--- diary/views.py
+++ diary/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from .models import Entry
 
 
 def index(request):
     return render(request, 'diary/index.html', {})
+
+
+def entry_list(request):
+    entries = Entry.objects.order_by('created_date')
+    context = {'entries': entries}
+    return render(request, 'diary/entry_list.html', context)
