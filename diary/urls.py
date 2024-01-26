3a1cba2829c1580fe5d4e5933c522be6657126fc
margareth91 <margerytka@wp.eu>
2024-01-26 13:25:47 +0100

    added new_entry url, view & template

--- diary/urls.py
+++ diary/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('entry_list/', views.entry_list, name='entry_list'),
     path('entry/<int:entry_id>/', views.entry_detail, name='entry_detail'),
+    path('entry/new_entry/', views.new_entry, name='new_entry'),
 ]
--- diary/views.py
+++ diary/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Entry
+from .forms import EntryForm
 
 
 def index(request):
@@ -16,3 +17,17 @@ def entry_detail(request, entry_id):
     entry = get_object_or_404(Entry, id=entry_id)
     context = {'entry': entry}
     return render(request, 'diary/entry_detail.html', context)
+
+
+def new_entry(request):
+    if request.method != 'POST':
+        form = EntryForm()
+    else:
+        form = EntryForm(request.POST)
+        if form.is_valid():
+            entry = form.save(commit=False)
+            entry.author = request.user
+            entry.save()
+            return redirect('entry_detail', entry_id=entry.id)
+    context = {'form': form}
+    return render(request, 'diary/new_entry.html', context)
