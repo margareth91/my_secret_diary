35df54e5d5213e9a02f249a5d79ef6771a6e5172
margareth91 <margerytka@wp.eu>
2024-01-29 15:20:18 +0100

    added login_required to app functions

--- diary/views.py
+++ diary/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
 
 from .forms import EntryForm
@@ -5,23 +6,26 @@ from .models import Entry
 
 
 def index(request):
-    return render(request, 'diary/index.html', {})
+    return render(request, "diary/index.html", {})
 
 
+@login_required
 def entry_list(request):
-    entries = Entry.objects.order_by('-created_date')
-    context = {'entries': entries}
-    return render(request, 'diary/entry_list.html', context)
+    entries = Entry.objects.filter(author=request.user).order_by("-created_date")
+    context = {"entries": entries}
+    return render(request, "diary/entry_list.html", context)
 
 
+@login_required
 def entry_detail(request, entry_id):
-    entry = get_object_or_404(Entry, id=entry_id)
-    context = {'entry': entry}
-    return render(request, 'diary/entry_detail.html', context)
+    entry = get_object_or_404(Entry, author=request.user, id=entry_id)
+    context = {"entry": entry}
+    return render(request, "diary/entry_detail.html", context)
 
 
+@login_required
 def new_entry(request):
-    if request.method != 'POST':
+    if request.method != "POST":
         form = EntryForm()
     else:
         form = EntryForm(request.POST)
@@ -29,14 +33,15 @@ def new_entry(request):
             entry = form.save(commit=False)
             entry.author = request.user
             entry.save()
-            return redirect('entry_detail', entry_id=entry.id)
-    context = {'form': form}
-    return render(request, 'diary/new_entry.html', context)
+            return redirect("entry_detail", entry_id=entry.id)
+    context = {"form": form}
+    return render(request, "diary/new_entry.html", context)
 
 
+@login_required
 def edit_entry(request, entry_id):
-    entry = get_object_or_404(Entry, id=entry_id)
-    if request.method != 'POST':
+    entry = get_object_or_404(Entry, author=request.user, id=entry_id)
+    if request.method != "POST":
         form = EntryForm(instance=entry)
     else:
         form = EntryForm(instance=entry, data=request.POST)
@@ -44,6 +49,6 @@ def edit_entry(request, entry_id):
             entry = form.save(commit=False)
             entry.author = request.user
             entry.save()
-            return redirect('entry_detail', entry_id=entry.id)
-    context = {'form': form, 'entry': entry}
-    return render(request, 'diary/edit_entry.html', context)
+            return redirect("entry_detail", entry_id=entry.id)
+    context = {"form": form, "entry": entry}
+    return render(request, "diary/edit_entry.html", context)
