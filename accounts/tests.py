a2bcdb108c28139f9bcdc3115320421156fafc6d
margareth91 <margerytka@wp.eu>
2024-04-09 13:18:38 +0200

    added test for user authentication

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
-# Create your tests here.
+from django.contrib.auth.models import User
+
+
+class AuthenticationTestCase(TestCase):
+    def setUp(self):
+        User.objects.create(username="testuser")
+
+    def test_user_created(self):
+        user = User.objects.filter(username="testuser")
+        self.assertTrue(user.exists())
