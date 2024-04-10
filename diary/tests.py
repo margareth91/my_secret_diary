b3328405065eb31fabfefb50e5e6ab69e20853c5
margareth91 <margerytka@wp.eu>
2024-04-10 18:59:45 +0200

    added tests for diary model entry

--- diary/tests.py
+++ diary/tests.py
@@ -1,3 +1,37 @@
+import datetime
+
+from django.contrib.auth.models import User
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Entry
+
+
+class EntryModelTest(TestCase):
+    ENTRY_TITLE = "Lorem Ipsum"
+    ENTRY_TEXT = "Lorem ipsum dolor sit amet."
+
+    def setUp(self):
+        self.author = User.objects.create()
+        Entry.objects.create(
+            author=self.author, title=self.ENTRY_TITLE, text=self.ENTRY_TEXT
+        )
+        self.entry = Entry.objects.get(id=1)
+
+    def test_entry_author(self):
+        self.assertTrue(self.entry.author, self.author)
+
+    def test_entry_title(self):
+        self.assertEqual(self.entry.title, self.ENTRY_TITLE)
+
+    def test_entry_text(self):
+        self.assertEqual(self.entry.text, self.ENTRY_TEXT)
+
+    def test_date_added_automatically(self):
+        self.assertTrue(type(self.entry.created_date), datetime)
+
+    def test_verbose_name_plural(self):
+        verbose_name_plural = self.entry._meta.verbose_name_plural
+        self.assertEqual(verbose_name_plural, "entries")
+
+    def test_str(self):
+        self.assertEqual(self.entry.__str__(), self.entry.title)
