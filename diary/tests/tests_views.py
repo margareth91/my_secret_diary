1e35c3523991c50366846db54be4b070f752cb02
margareth91 <margerytka@wp.eu>
2024-04-12 15:22:19 +0200

    added tests for entry_detail view

--- diary/tests/tests_views.py
+++ diary/tests/tests_views.py
@@ -2,6 +2,8 @@ from django.contrib.auth.models import User
 from django.test import TestCase, Client
 from django.urls import reverse
 
+from diary.models import Entry
+
 
 class IndexViewTest(TestCase):
     def setUp(self):
@@ -33,3 +35,30 @@ class EntryListTest(TestCase):
         self.client.login(username="user", password="password")
         response = self.client.get(reverse("entry_list"))
         self.assertTemplateUsed(response, "diary/entry_list.html")
+
+
+class EntryDetailViewTest(TestCase):
+    ENTRY_TITLE = "Lorem Ipsum"
+    ENTRY_TEXT = "Lorem ipsum dolor sit amet."
+
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(username="user", password="password")
+        Entry.objects.create(
+            author=self.user, title=self.ENTRY_TITLE, text=self.ENTRY_TEXT
+        )
+        self.entry = Entry.objects.get(id=1)
+
+    def redirect_not_logged_user(self):
+        response = self.client.get(reverse("entry_detail"))
+        self.assertEqual(response.status_code, 302)
+
+    def get_entry_detail_logged_user(self):
+        self.client.login(username="user", password="password")
+        response = self.client.get(reverse("entry_detail"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_entry_detail_correct_template(self):
+        self.client.login(username="user", password="password")
+        response = self.client.get(reverse("entry_detail", args=[1]))
+        self.assertTemplateUsed(response, "diary/entry_detail.html")
