4a29c42d3d437b900aaa372f9926b1969486fdf0
margareth91 <margerytka@wp.eu>
2024-04-12 13:57:42 +0200

    added additional test for index view

--- diary/tests/tests_views.py
+++ diary/tests/tests_views.py
@@ -4,7 +4,10 @@ from django.test import TestCase, Client
 class IndexViewTest(TestCase):
     def setUp(self):
         self.client = Client()
+        self.response = self.client.get("")
 
     def test_get_index(self):
-        response = self.client.get("")
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_index_correct_template(self):
+        self.assertTemplateUsed(self.response, "diary/index.html")
