239e43040a1512a9cdd68bc92323779e68cca5c8
margareth91 <margerytka@wp.eu>
2024-04-15 14:10:00 +0200

    added test for logout view

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,6 +1,6 @@
-from django.test import TestCase
-
 from django.contrib.auth.models import User
+from django.test import TestCase, Client
+from django.urls import reverse
 
 
 class AuthenticationTestCase(TestCase):
@@ -10,3 +10,14 @@ class AuthenticationTestCase(TestCase):
     def test_user_created(self):
         user = User.objects.filter(username="testuser")
         self.assertTrue(user.exists())
+
+
+class LogoutViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(username="user", password="password")
+
+    def test_logout(self):
+        self.client.login(username="user", password="password")
+        response = self.client.get(reverse("logout"))
+        self.assertEqual(response.status_code, 302)
